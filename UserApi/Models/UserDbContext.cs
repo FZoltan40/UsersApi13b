ebf5df58f316a2d29887d1df8ab6dde3c58b8037
Farkas <farasz@kkszki.hu>
2024-10-08 11:31:58 +0200

    DbContext file is ready to use EtityFramework.

---
+++ UserApi/Models/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserApi.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext() { }
+
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=Employee; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+
+        public DbSet<User> NewUser { get; set; } = null!;
+    }
+}
--- UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
+++ UserApi/obj/Debug/net8.0/UserApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bde35c9f3295edc2e1b773c27987a0514c859b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
